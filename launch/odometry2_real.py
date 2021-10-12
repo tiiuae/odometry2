cf2dd3eac434cc14367ba6f4b3c89bda1ab37c3b
stibipet <stibipet@fel.cvut.cz>
2021-10-12 19:10:55 +0200

    update launchfile

--- launch/odometry2_real.py
+++ launch/odometry2_real.py
@@ -33,7 +33,7 @@ def generate_launch_description():
                 namespace=namespace,
                 name='odometry2',
                 parameters=[
-                    pkg_share_path + '/config/odometry2.yaml',
+                    pkg_share_path + '/config/odometry2_real.yaml',
                     {"use_sim_time": launch.substitutions.LaunchConfiguration("use_sim_time")},
                 ],
                 remappings=[
